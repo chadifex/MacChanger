03092b906b92aacbc477ecd466a485ff16826566
chadifex <maxcoates@icloud.com>
2023-05-09 11:03:22 +0100

    fixed vulnerability where user can execute unwanted commands using the script.

--- MACchanger.py
+++ MACchanger.py
@@ -1,17 +1,24 @@
 # MAC Address Changer Script
 # ctrl + / comments all highlighted lines
 import subprocess # w library
+import optparse
 
+parser = optparse.OptionParser() # object to handle CLI args
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's MAC Address") # dest = where value goes
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address") #dest = destinaton variable
 
-interface = input("enter interface: ") # for python2
-new_mac = input("enter new mac: ") # do "raw_input" instead of "input"
+(options, arguments) = parser.parse_args() #passes all add_option to options, arguments variables
+# options contains input, arguments contains --interface, --mac etc
+
+interface = options.interface
+new_mac = options.new_mac
 print("[+] Changing MAC Address for: " + interface + " to: " + new_mac)
 
-subprocess.call("ifconfig " + interface, shell=True) #see old mac
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
-subprocess.call("ifconfig " + interface, shell=True) #see changed mac
+subprocess.call(["ifconfig", interface]) #see old mac
+subprocess.call(["ifconfig", interface, "down"]) # no + but , instead
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface + "up"]) # no space between "" as not using shell=True does it auto
+subprocess.call(["ifconfig", interface]) #see changed mac
 print(new_mac) #extra verification
 # ctrl + d pastes line below
 # changes mac address to 00:11:22:33:44:66
